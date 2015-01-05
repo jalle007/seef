c7aa2935d89ebeddba2e315c837584d92a4a8f2d
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2015-01-05 09:59:44 +0100

    readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-seef
-====
+Seef video player
+================
 
-seef video player
+Video payer who demonstrates launch another video when the user clicks on the hotspot.
+
+Usage: click anywhere to start video. 
+       click on a hotspot to play another video.
--- app/src/main/java/seef/jalle/com/seef/MainActivity.java
+++ app/src/main/java/seef/jalle/com/seef/MainActivity.java
@@ -3,10 +3,6 @@ package seef.jalle.com.seef;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
@@ -21,10 +17,9 @@ public class MainActivity extends Activity {
     MediaController mediaController;
     private ProgressDialog progressDialog;
     private boolean firstPlay = true;
-//    float leftEdge = (float) 0.3539 *width;
-//    float rightEdge = (float) 0.8547*width;
-//    float topEdge = 0;
-//    float bottomEdge =  (float)0.603
+    View hotspot ;
+    float width,height;
+    float leftEdge, rightEdge , topEdge , bottomEdge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +31,9 @@ public class MainActivity extends Activity {
         mediaController.setAnchorView(videoView);
 
         openVideo("footagea");
-        drawRect();
+
+//        hotspot=(View)findViewById(R.id.hotspot);
+//        drawRect();
 //
 //        final ImageButton imgPlay=(ImageButton) findViewById(R.id.imgPlay);
 //        imgPlay.setOnClickListener(new View.OnClickListener(){
@@ -60,47 +57,56 @@ public class MainActivity extends Activity {
                     if (isHotspot(motionEvent)) {
                         openVideo("footageb");
                         videoView.start();
+                        String msg = "playing footage b";
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
                     }
                     return false;
                 }
             }
         });
+
+
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        super.onWindowFocusChanged(hasFocus);
+
+//        width =videoView.getWidth();
+//        height  =videoView.getWidth();
+//        leftEdge = (float) 0.3539 * width;
+//        rightEdge = (float) 0.8547 * width;
+//        topEdge = 0;
+//        bottomEdge = (float) 0.603 * height;
+//
+//        if (firstPlay)
+//        {
+//            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) hotspot.getLayoutParams();
+//            layoutParams.leftMargin = (int) leftEdge;
+//            layoutParams.topMargin = (int) topEdge;
+//            layoutParams.rightMargin = (int) (rightEdge -width);
+//            layoutParams.bottomMargin =(int) (bottomEdge-height);
+//            hotspot.setLayoutParams(layoutParams);
+//            hotspot.setVisibility(View.VISIBLE);
+//        }
+    }
     private void drawRect() {
-        Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
-                .getDefaultDisplay().getWidth(), (int) getWindowManager()
-                .getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-
-        // Rectangle
-
-        Paint paint = new Paint();
-        paint.setColor(Color.GREEN);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setStrokeWidth(10);
-        float leftx = 20;
-        float topy = 20;
-        float rightx = 250;
-        float bottomy = 200;
-        canvas.drawRect(leftx, topy, rightx, bottomy, paint);
+        hotspot.setVisibility(View.VISIBLE);
     }
 
     private boolean isHotspot(MotionEvent motionEvent) {
-        float width = videoView.getWidth();
-        float height = videoView.getHeight();
         float posx = motionEvent.getX();
         float posy = motionEvent.getY();
 
-        float leftEdge = (float) 0.3539 * width;
-        float rightEdge = (float) 0.8547 * width;
-        float topEdge = 0;
-        float bottomEdge = (float) 0.603 * height;
+        width = videoView.getWidth();
+        height = videoView.getHeight();
+        leftEdge = (float) 0.3539 * width;
+        rightEdge = (float) 0.8547 * width;
+        topEdge = 0;
+        bottomEdge = (float) 0.603 * height;
 
         boolean isIn = posx > leftEdge && posx < rightEdge && posy < bottomEdge && posy > topEdge;
-
-        String msg = String.valueOf(isIn);
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
         return isIn;
     }
 
@@ -113,7 +119,7 @@ public class MainActivity extends Activity {
         progressDialog.show();
     }
 
-    private void openVideo(String file) {
+    public void openVideo(String file) {
         setProgress();
 
         if (videoView.isPlaying()) { videoView.stopPlayback(); }
@@ -128,9 +134,9 @@ public class MainActivity extends Activity {
         videoView.setVideoURI(uri);
         videoView.seekTo(100);
         videoView.requestFocus();
-        videoView.pause();
         progressDialog.dismiss();
         //  videoView.start();
+
     }
 
     @Override
